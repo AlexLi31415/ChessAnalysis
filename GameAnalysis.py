9cc06650efc0413d325d6ae6c00e2754dc026037
Alex Li <alexli@Alexs-MacBook-Air.local>
2024-04-01 23:33:15 -0500

    Use chess module, add win_prob_lichess

--- GameAnalysis.py
+++ GameAnalysis.py
@@ -1,14 +1,11 @@
-import asyncio
+import pandas as pd
+from math import exp
+import numpy as np
+import scipy.stats as sps
+import matplotlib.pyplot as plt
+import chess
+import chess.pgn
+import chess.engine
 
-from chessdotcom.aio import get_player_profile
-
-def get_player_games(user):
-
-    player = get_player_profile(user)
-
-    async def gather_cors(player):
-        return await asyncio.gather(player)
-
-    responses = asyncio.run(gather_cors(player))
-    
-    return responses
+def win_prob_lichess(centipawns):
+    return 0.5 + 0.5 * (2 / (1 + exp(-0.00368208 * centipawns)) - 1)
